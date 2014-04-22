80828bccc57f49d8037cafe967c08159a97b2c25
xiaoping jin <jinxp@lenovo.com>
2014-04-22 15:51:02 +0800

    update ...

--- Demo/src/com/talk/demo/account/LoginActivity.java
+++ Demo/src/com/talk/demo/account/LoginActivity.java
@@ -67,14 +67,12 @@ public class LoginActivity extends Activity {
 			public void beforeTextChanged(CharSequence s, int start, int count,
 					int after) {
 				// TODO Auto-generated method stub
-				
 			}
 
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
 				// TODO Auto-generated method stub
-				
 			}
 		};
 		loginText.addTextChangedListener(watcher);
