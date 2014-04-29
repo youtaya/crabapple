1b49669afcf0c8019649b180fec2dbf069112225
youtaya <jxiaoping@gmail.com>
2014-04-29 23:37:47 +0800

    update...

--- Demo/src/com/talk/demo/daily/DailyEditActivity.java
+++ Demo/src/com/talk/demo/daily/DailyEditActivity.java
@@ -1,15 +1,23 @@
 package com.talk.demo.daily;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.talk.demo.MainActivity;
 import com.talk.demo.R;
+import com.talk.demo.persistence.DBManager;
+import com.talk.demo.persistence.TimeRecord;
+import com.talk.demo.util.TalkUtil;
 
 public class DailyEditActivity extends Activity {
 	private static String TAG = "DailyEditActivity";
 	private EditText edit_content;
 	private String pre_content;
+	private DBManager mgr;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +27,41 @@ public class DailyEditActivity extends Activity {
         pre_content = bundle.getString("precontent");
         edit_content = (EditText)findViewById(R.id.edit_content);
         edit_content.setText(pre_content);
+        
+        mgr = new DBManager(this);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.confirm_actions, menu);
+        return true;
+    }
+    
+    private void confirmDone() {
+    	//save to db
+        String content = edit_content.getText().toString();
+        // Do nothing if content is empty
+        if(content.length() > 0) {
+        	TimeRecord tr = new TimeRecord(content);  
+        	tr.setContentType(TalkUtil.MEDIA_TYPE_TEXT);
+        	mgr.add(tr);
+	    }
+        //goto main activity
+        Intent mIntent = new Intent();
+        mIntent.setClass(this, MainActivity.class);
+        startActivity(mIntent);
+        finish();
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_confirm:
+                confirmDone();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
