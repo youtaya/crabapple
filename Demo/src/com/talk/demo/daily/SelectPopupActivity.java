53a77e0b210a40e0ece9064d11e85db0283458ed
youtaya <jxiaoping@gmail.com>
2014-08-28 23:39:10 +0800

    update

--- Demo/src/com/talk/demo/daily/SelectPopupActivity.java
+++ Demo/src/com/talk/demo/daily/SelectPopupActivity.java
@@ -7,8 +7,6 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
 
 import com.talk.demo.R;
 
