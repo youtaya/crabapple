f160534aad066331232fde1a4b648aba2410a076
xiaoping jin <jinxp@lenovo.com>
2014-09-04 14:22:39 +0800

    update

--- Demo/src/com/talk/demo/daily/SelectTagActivity.java
+++ Demo/src/com/talk/demo/daily/SelectTagActivity.java
@@ -1,153 +1,53 @@
 package com.talk.demo.daily;
 
+import android.R.integer;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.BaseExpandableListAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ExpandableListView.OnGroupClickListener;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.talk.demo.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class SelectTagActivity extends Activity {
 	private static final String TAG = "SelectTagActivity";
-    private List<String> GroupData;//定义组数据  
-    private List<List<String>> ChildrenData;//定义组中的子数据
-    private void LoadListDate() {  
-        GroupData = new ArrayList<String>();  
-        GroupData.add("想你的夜");  
-        GroupData.add("月亮代表我的心");  
-        GroupData.add("下一站天后");  
-  
-        ChildrenData = new ArrayList<List<String>>();  
-        List<String> Child1 = new ArrayList<String>();  
-        Child1.add("2012年6月21日");  
-        Child1.add("2012年6月22日");
-        Child1.add("2012年6月23日");
-        ChildrenData.add(Child1);  
-        List<String> Child2 = new ArrayList<String>();  
-        Child2.add("喜马拉雅");  
-        Child2.add("泰山");  
-        Child2.add("嵩山");  
-        ChildrenData.add(Child2);  
-        List<String> Child3 = new ArrayList<String>();  
-        Child3.add("Sahara");  
-        Child3.add("Egypt");  
-        ChildrenData.add(Child3);  
-    }  
+	private ArrayList<String> listItems = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_tag);
-        LoadListDate();  
+		setContentView(R.layout.activity_select_tag);
         
-        ExpandableListView myExpandableListView = (ExpandableListView)findViewById(R.id.expandableListView);  
-        myExpandableListView.setAdapter(new ExpandableAdapter());
-        myExpandableListView.setOnGroupClickListener(new OnGroupClickListener() {
-
-			@Override
-			public boolean onGroupClick(ExpandableListView parent, View v,
-					int groupPosition, long id) {
-				Log.d(TAG, "group postion : "+groupPosition);
-				String tag = parent.getExpandableListAdapter().getGroup(groupPosition).toString();
-				Log.d(TAG, "parent : "+tag);
-				Intent resultIntent = new Intent();
-		        resultIntent.putExtra("tag_name", tag);
-		        setResult(RESULT_OK, resultIntent);
-		    	finish();
-				return false;
-			}
-        	
-        });
+        ListView myListView = (ListView)findViewById(R.id.ListView);
+        adapter=new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                listItems);
+        myListView.setAdapter(adapter);
 	
 	}
 	
-    private class ExpandableAdapter extends BaseExpandableListAdapter {  
-		@Override
-		public Object getChild(int groupPosition, int childPosition) {
-			return ChildrenData.get(groupPosition).get(childPosition);
-		}
-
-		@Override
-		public long getChildId(int groupPosition, int childPosition) {
-			return 0;
-		}
-
-		@Override
-		public View getChildView(int groupPosition, int childPosition,
-				boolean isLastChild, View convertView, ViewGroup parent) {
-            TextView myText = null;  
-            if (convertView != null) {  
-                myText = (TextView)convertView;  
-                myText.setText(ChildrenData.get(groupPosition).get(childPosition));  
-            } else {  
-                myText = createView(ChildrenData.get(groupPosition).get(childPosition));  
-            }
-            return myText;  
-		}
-
-		@Override
-		public int getChildrenCount(int groupPosition) {
-			return ChildrenData.get(groupPosition).size();
-		}
-
-		@Override
-		public Object getGroup(int groupPosition) {
-			return GroupData.get(groupPosition);
-		}
-
-		@Override
-		public int getGroupCount() {
-			return GroupData.size();
-		}
-
-		@Override
-		public long getGroupId(int groupPosition) {
-			return 0;
-		}
-
-		@Override
-		public View getGroupView(int groupPosition, boolean isExpanded,
-				View convertView, ViewGroup parent) {
-            TextView myText = null;  
-            if (convertView != null) {  
-                myText = (TextView)convertView;  
-                myText.setText(GroupData.get(groupPosition));  
-            } else {  
-                myText = createView(GroupData.get(groupPosition));  
-            }  
-            return myText;
-		}
-
-		@Override
-		public boolean hasStableIds() {
-			return false;
-		}
-
-		@Override
-		public boolean isChildSelectable(int groupPosition, int childPosition) {
-			return false;
-		}
-		
-        private TextView createView(String content) {  
-            AbsListView.LayoutParams layoutParams = new AbsListView.LayoutParams(    
-                    ViewGroup.LayoutParams.FILL_PARENT, 80);    
-            TextView myText = new TextView(SelectTagActivity.this);    
-            myText.setLayoutParams(layoutParams);    
-            myText.setGravity(Gravity.CENTER_VERTICAL | Gravity.LEFT);    
-            myText.setPadding(80, 0, 0, 0);    
-            myText.setText(content);  
-            return myText;  
-        }
+    public void addItems(View v) {
+        listItems.add("Clicked : "+clickCounter++);
+        adapter.notifyDataSetChanged();
     }
-
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_tag, menu);
+        return true;
+    }
+	
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        
+        if(id == android.R.id.action_add) {
+            //TODO: add tag
+            return true;
+        }
+        
+        return super.onOptionsItemSelected(item);
+    }
 }
