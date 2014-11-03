6ab14fbbbf34b9d3851f98f210553c6557d1bffa
xiaoping jin <jinxp@lenovo.com>
2014-11-03 11:42:01 +0800

    update

--- Demo/src/com/talk/demo/time/ViewItemActivity.java
+++ Demo/src/com/talk/demo/time/ViewItemActivity.java
@@ -1,3 +1,4 @@
+
 package com.talk.demo.time;
 
 import android.app.Activity;
@@ -19,17 +20,16 @@ public class ViewItemActivity extends Activity {
     private String create_time;
     private String create_date;
     private String content;
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_view_item);
-	        Bundle bundle = getIntent().getExtras();
-	        create_time = bundle.getString("createtime");
-	        create_date = bundle.getString("createdate");
-	        content = bundle.getString("content");
-	        
-		}
-		
-	}
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_item);
+        Bundle bundle = getIntent().getExtras();
+        create_time = bundle.getString("createtime");
+        create_date = bundle.getString("createdate");
+        content = bundle.getString("content");
 
+    }
+
+}
