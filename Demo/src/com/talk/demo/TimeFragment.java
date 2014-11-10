404b6461af674e9289a1471d411a134df008c471
youtaya <jxiaoping@gmail.com>
2014-11-11 00:50:04 +0800

    update

--- Demo/src/com/talk/demo/TimeFragment.java
+++ Demo/src/com/talk/demo/TimeFragment.java
@@ -13,8 +13,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
+import com.fortysevendeg.swipelistview.BaseSwipeListViewListener;
+import com.fortysevendeg.swipelistview.SwipeListView;
 import com.talk.demo.account.AccountConstants;
 import com.talk.demo.core.RecordManager;
 import com.talk.demo.persistence.RecordCache;
@@ -24,12 +25,11 @@ import com.talk.demo.util.AccountUtils;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
-import java.util.Map;
 
 public class TimeFragment extends Fragment {
     
     private static String TAG = "TimeFragment";
-    private ListView lv;
+    private SwipeListView lv;
     private ArrayList<TimeViewItem> time_record;
     private HashMap<String, ArrayList<RecordCache>> record_cache;
     private TimeListAdapter tAdapter;
@@ -46,9 +46,81 @@ public class TimeFragment extends Fragment {
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_time, container, false);
         
-        lv = (ListView)rootView.findViewById(R.id.time_list);
-        
+        lv = (SwipeListView)rootView.findViewById(R.id.time_list);
+
         initListView();
+        
+        lv.setSwipeListViewListener(new BaseSwipeListViewListener() {
+            @Override
+            public void onChoiceChanged(int position, boolean selected) {
+                Log.d(TAG, "onChoiceChanged:" + position + ", " + selected);
+            }
+
+            @Override
+            public void onChoiceEnded() {
+                Log.d(TAG, "onChoiceEnded");
+            }
+
+            @Override
+            public void onChoiceStarted() {
+                Log.d(TAG, "onChoiceStarted");
+            }
+
+            @Override
+            public void onClickBackView(int position) {
+                Log.d(TAG, "onClickBackView:" + position);
+            }
+
+            @Override
+            public void onClickFrontView(int position) {
+                Log.d(TAG, "onClickFrontView:" + position);
+            }
+
+            @Override
+            public void onClosed(int position, boolean fromRight) {
+                Log.d(TAG, "onClosed:" + position + "," + fromRight);
+            }
+
+            @Override
+            public void onDismiss(int[] arg0) {
+                Log.d(TAG, "onDismiss");
+            }
+
+            @Override
+            public void onFirstListItem() {
+                Log.d(TAG, "onFirstListItem");
+            }
+
+            @Override
+            public void onLastListItem() {
+                Log.d(TAG, "onLastListItem");
+            }
+
+            @Override
+            public void onListChanged() {
+                Log.d(TAG, "onListChanged");
+            }
+
+            @Override
+            public void onMove(int position, float x) {
+                Log.d(TAG, "onMove:" + position + "," + x);
+            }
+
+            @Override
+            public void onOpened(int position, boolean toRight) {
+                Log.d(TAG, "onOpened:" + position + "," + toRight);
+            }
+
+            @Override
+            public void onStartClose(int position, boolean right) {
+                Log.d(TAG, "onStartClose:" + position + "," + right);
+            }
+
+            @Override
+            public void onStartOpen(int position, int action, boolean right) {
+                Log.d(TAG, "onStartOpen:" + position + "," + action + "," + right);
+            }
+        });
         
         return rootView;
     }
