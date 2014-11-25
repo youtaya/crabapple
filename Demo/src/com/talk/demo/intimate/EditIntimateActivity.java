a2ee0d7f32b13dc4788a381c8ade3785e6e424b8
youtaya <jxiaoping@gmail.com>
2014-11-25 23:56:25 +0800

    update

--- Demo/src/com/talk/demo/intimate/EditIntimateActivity.java
+++ Demo/src/com/talk/demo/intimate/EditIntimateActivity.java
@@ -1,14 +1,27 @@
 package com.talk.demo.intimate;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.talk.demo.R;
 
+import net.sectorsieteg.avatars.AvatarDrawableFactory;
+
 public class EditIntimateActivity extends Activity {
 	private String friend_name;
 	private TextView tvFriendName;
+	private ImageView ivAvatar;
+	private TextView tvLinkageTo, tvLinkageFrom;
+	private TextView tvNumberTo, tvNumberFrom;
+	private TextView tvChannel;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +31,32 @@ public class EditIntimateActivity extends Activity {
         friend_name = bundle.getString("friend");
         tvFriendName = (TextView) findViewById(R.id.friend_name);
         tvFriendName.setText(friend_name);
+        
+        ivAvatar = (ImageView) findViewById(R.id.friend_avatar);
+        
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = false;
+        Bitmap avatar = BitmapFactory.decodeResource(this.getResources(), R.drawable.avatar, options);
+
+        AvatarDrawableFactory avatarDrawableFactory = new AvatarDrawableFactory(this.getResources());
+        Drawable roundedAvatarDrawable = avatarDrawableFactory.getRoundedAvatarDrawable(avatar);
+        ivAvatar.setImageDrawable(roundedAvatarDrawable);
+        
+        tvLinkageTo = (TextView) findViewById(R.id.linkage_from);
+        tvNumberTo = (TextView) findViewById(R.id.number_from);
+        tvLinkageTo.setText("我对"+friend_name+"亲密度");
+        tvLinkageFrom = (TextView) findViewById(R.id.linkage_to);
+        tvLinkageFrom.setText(friend_name+"对我亲密度");
+        tvNumberFrom = (TextView) findViewById(R.id.number_to);
+        
+        tvChannel = (TextView) findViewById(R.id.subject_wall);
+        tvChannel.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(EditIntimateActivity.this, IntimateChannelActivity.class));
+			}
+        	
+        });
     }
 }
