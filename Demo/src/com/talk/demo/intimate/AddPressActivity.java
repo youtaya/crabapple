830587224fa2d37046dc8423dbe1fc22ea06df4c
youtaya <jxiaoping@gmail.com>
2014-11-30 23:26:39 +0800

    update

--- Demo/src/com/talk/demo/intimate/AddPressActivity.java
+++ Demo/src/com/talk/demo/intimate/AddPressActivity.java
@@ -1,7 +1,10 @@
 package com.talk.demo.intimate;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
@@ -22,13 +25,30 @@ public class AddPressActivity extends Activity {
 			"Think It",
 			"More, More"
 	};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_press);
         
+        me_step = (Button) findViewById(R.id.step_some);
+        me_step.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(AddPressActivity.this, EditIntimateActivity.class); 
+				Bundle bundle = new Bundle();
+				bundle.putInt("STEP", 1);
+				intent.putExtras(bundle);
+	            setResult(RESULT_OK, intent);
+	    		finish();
+			}
+        	
+        });
         gridView = (GridView) findViewById(R.id.ment_content);
         mentAdapter = new MentGridViewAdapter(AddPressActivity.this, contents);
         gridView.setAdapter(mentAdapter);
     }
+    
+
 }
--- Demo/src/com/talk/demo/intimate/EditIntimateActivity.java
+++ Demo/src/com/talk/demo/intimate/EditIntimateActivity.java
@@ -5,17 +5,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.talk.demo.R;
+import com.talk.demo.util.TalkUtil;
 
 import net.sectorsieteg.avatars.AvatarDrawableFactory;
 
 public class EditIntimateActivity extends Activity {
+	private static String TAG = "EditIntimateActivity";
 	private String friend_name;
 	private TextView tvFriendName;
 	private ImageView ivAvatar;
@@ -23,6 +27,10 @@ public class EditIntimateActivity extends Activity {
 	private TextView tvNumberTo, tvNumberFrom;
 	private TextView tvPressAdd;
 	private TextView tvChannel;
+	private int numberTo;
+	private int oneStep;
+
+	private static final int ONE_STEP = 29;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +40,7 @@ public class EditIntimateActivity extends Activity {
         friend_name = bundle.getString("friend");
         tvFriendName = (TextView) findViewById(R.id.friend_name);
         tvFriendName.setText(friend_name);
-        
+
         ivAvatar = (ImageView) findViewById(R.id.friend_avatar);
         
         BitmapFactory.Options options = new BitmapFactory.Options();
@@ -44,11 +52,14 @@ public class EditIntimateActivity extends Activity {
         ivAvatar.setImageDrawable(roundedAvatarDrawable);
         
         tvLinkageTo = (TextView) findViewById(R.id.linkage_from);
-        tvNumberTo = (TextView) findViewById(R.id.number_from);
+        tvNumberTo = (TextView) findViewById(R.id.number_to);
+        numberTo = 61;
+        tvNumberTo.setText(String.valueOf(numberTo));
+        
         tvLinkageTo.setText("我对"+friend_name+"亲密度");
         tvLinkageFrom = (TextView) findViewById(R.id.linkage_to);
         tvLinkageFrom.setText(friend_name+"对我亲密度");
-        tvNumberFrom = (TextView) findViewById(R.id.number_to);
+        tvNumberFrom = (TextView) findViewById(R.id.number_from);
         
         tvPressAdd = (TextView) findViewById(R.id.add_press);
         tvPressAdd.setOnClickListener(new OnClickListener() {
@@ -56,7 +67,7 @@ public class EditIntimateActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				Intent newIntent = new Intent(EditIntimateActivity.this, AddPressActivity.class);
-				startActivityForResult(newIntent, 0);
+				startActivityForResult(newIntent, ONE_STEP);
 			}
         	
         });
@@ -71,4 +82,21 @@ public class EditIntimateActivity extends Activity {
         	
         });
     }
+    
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d(TAG, "got the return :" + requestCode + " :" + resultCode);
+		switch (requestCode) {
+		case ONE_STEP:
+			if (resultCode == RESULT_OK) {
+				Bundle extras = data.getExtras();
+				oneStep = extras.getInt("STEP");
+				
+		        numberTo += oneStep;
+		        tvNumberTo.setText(String.valueOf(numberTo));
+			}
+			break;
+		}
+	}
+    
 }
