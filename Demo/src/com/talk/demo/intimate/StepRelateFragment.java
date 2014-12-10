32eef61d2b1012942334b0d17fa6a027be5be188
youtaya <jxiaoping@gmail.com>
2014-12-11 01:16:52 +0800

    update

--- Demo/src/com/talk/demo/intimate/StepRelateFragment.java
+++ Demo/src/com/talk/demo/intimate/StepRelateFragment.java
@@ -3,8 +3,10 @@ package com.talk.demo.intimate;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.util.JsonWriter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,7 +35,6 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -44,56 +45,57 @@ import java.util.List;
 import java.util.Scanner;
 
 public class StepRelateFragment extends Fragment {
-	
+
+	private static final String TAG = "StepRelateFragment";
 	private Button saveButton;
 	private Context mContext;
 	private List<LatLng> tempData;
 	private File tempFile;
+	boolean isFirstRead = true;
 	// 定位相关
 	LocationClient mLocClient;
 	public MyLocationListenner myListener = new MyLocationListenner();
 	private LocationMode mCurrentMode;
 	BitmapDescriptor mCurrentMarker;
-	
-    MapView mMapView = null;  // 定义mapview对象
-    BaiduMap mBaiduMap = null;
-    
+
+	MapView mMapView = null; // 定义mapview对象
+	BaiduMap mBaiduMap = null;
+
 	boolean isFirstLoc = true;// 是否首次定位
 	private SharedPreferences settings;
 	private double lat, lng;
-	
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-        Bundle savedInstanceState) {
-    	
-    	SDKInitializer.initialize(getActivity().getApplicationContext()); 
-    	
-    	settings = getActivity().getSharedPreferences("GeoPoint_Info",0);
-    	lat = (double)settings.getLong("Lat", 0);
-    	lng = (double)settings.getLong("Lng", 0);
-        // Inflate the layout for this fragment
-    	View rootView = inflater.inflate(R.layout.fragment_relate_step, container, false);
-    	mMapView = (MapView)rootView.findViewById(R.id.mapview);
-    	mBaiduMap = mMapView.getMap();  
-    	//普通地图  
-    	mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
-    	
-    	// 开启定位图层  
-    	mBaiduMap.setMyLocationEnabled(true);
-    	
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		SDKInitializer.initialize(getActivity().getApplicationContext());
+
+		settings = getActivity().getSharedPreferences("GeoPoint_Info", 0);
+		lat = (double) settings.getLong("Lat", 0);
+		lng = (double) settings.getLong("Lng", 0);
+		// Inflate the layout for this fragment
+		View rootView = inflater.inflate(R.layout.fragment_relate_step,
+				container, false);
+		mMapView = (MapView) rootView.findViewById(R.id.mapview);
+		mBaiduMap = mMapView.getMap();
+		// 普通地图
+		mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+
+		// 开启定位图层
+		mBaiduMap.setMyLocationEnabled(true);
+
 		// 修改为自定义marker
-    	mCurrentMode = LocationMode.NORMAL;
+		mCurrentMode = LocationMode.NORMAL;
 		mCurrentMarker = BitmapDescriptorFactory
 				.fromResource(R.drawable.icon_geo);
-		mBaiduMap
-				.setMyLocationConfigeration(new MyLocationConfiguration(
-						mCurrentMode, true, mCurrentMarker));
-		
-		MyLocationData defaultData = new MyLocationData.Builder()
-			.latitude(lat)
-			.longitude(lng).build();
+		mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+				mCurrentMode, true, mCurrentMarker));
+
+		MyLocationData defaultData = new MyLocationData.Builder().latitude(lat)
+				.longitude(lng).build();
 		mBaiduMap.setMyLocationData(defaultData);
-		
+
 		// 定位初始化
 		mLocClient = new LocationClient(getActivity());
 		mLocClient.registerLocationListener(myListener);
@@ -103,15 +105,26 @@ public class StepRelateFragment extends Fragment {
 		option.setScanSpan(1000);
 		mLocClient.setLocOption(option);
 		mLocClient.start();
-		
-		tempFile = new File(mContext.getExternalCacheDir(), "testJson.json");
+
+		File tempDir = new File(Environment.getExternalStorageDirectory()
+				+ "/Demotest");
+		if (!tempDir.exists()) {
+			tempDir = new File("/sdcard/Demo");
+			tempDir.mkdir();
+		}
+
+		tempFile = new File(tempDir, "Json.json");
 		try {
-			tempData = getLocations(new FileInputStream(tempFile));
+			if(isFirstRead) {
+				isFirstRead = false;
+			} else {
+				tempData = getLocations(new FileInputStream(tempFile));
+			}
 		} catch (IOException ex) {
 			// TODO Auto-generated catch block
 			ex.printStackTrace();
 		}
-		saveButton = (Button)rootView.findViewById(R.id.bt_save_publish);
+		saveButton = (Button) rootView.findViewById(R.id.bt_save_publish);
 		saveButton.setOnClickListener(new OnClickListener() {
 
 			@Override
@@ -124,43 +137,55 @@ public class StepRelateFragment extends Fragment {
 					e.printStackTrace();
 				}
 			}
-			
+
 		});
-    	return rootView;
-    }
-    
-    public void writeJsonStream(OutputStream out, List<LatLng> messages) throws IOException {
-        JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
-        writeMessagesArray(writer, messages);
-        writer.close();
-    }
+		return rootView;
+	}
 
-      public void writeMessagesArray(JsonWriter writer, List<LatLng> mesgs) throws IOException {
-        writer.beginArray();
-        for (LatLng message : mesgs) {
-          writeMessage(writer, message);
-        }
-        //record this step place
-        writeThisStep(writer);
-        
-        writer.endArray();
-      }
+	public void writeJsonStream(OutputStream out, List<LatLng> messages)
+			throws IOException {
+		JsonWriter writer = new JsonWriter(new OutputStreamWriter(out, "UTF-8"));
+		writeMessagesArray(writer, messages);
+		writer.close();
+		
+		out.flush();
+		out.close();
+	}
 
-      public void writeMessage(JsonWriter writer, LatLng message) throws IOException {
-        writer.beginObject();
-        writer.name("lng").value(message.longitude);
-        writer.name("lat").value(message.latitude);
-        writer.endObject();
-      }
-      
-      public void writeThisStep(JsonWriter writer) throws IOException {
-          writer.beginObject();
-          writer.name("lng").value(lng);
-          writer.name("lat").value(lat);
-          writer.endObject();
-        }
-      
-  	private List<LatLng> getLocations(InputStream input) {
+	public void writeMessagesArray(JsonWriter writer, List<LatLng> mesgs)
+			throws IOException {
+		
+		writer.beginArray();
+		if(mesgs == null) {
+			Log.e(TAG, "list is empty, skip it");
+		} else {
+			for (LatLng message : mesgs) {
+				writeMessage(writer, message);
+			}
+		}
+		// record this step place
+		writeThisStep(writer);
+
+		writer.endArray();
+	}
+
+	public void writeMessage(JsonWriter writer, LatLng message)
+			throws IOException {
+		writer.beginObject();
+		writer.name("lng").value(message.longitude);
+		writer.name("lat").value(message.latitude);
+		writer.endObject();
+	}
+
+	public void writeThisStep(JsonWriter writer) throws IOException {
+		writer.beginObject();
+		writer.name("lng").value(lng);
+		writer.name("lat").value(lat);
+		writer.endObject();
+	}
+
+	private List<LatLng> getLocations(InputStream input) 
+			throws IOException{
 		List<LatLng> list = new ArrayList<LatLng>();
 		String json = new Scanner(input).useDelimiter("\\A").next();
 		JSONArray array;
@@ -176,8 +201,12 @@ public class StepRelateFragment extends Fragment {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		input.close();
+		
 		return list;
 	}
+
 	/**
 	 * 定位SDK监听函数
 	 */
@@ -207,35 +236,34 @@ public class StepRelateFragment extends Fragment {
 		public void onReceivePoi(BDLocation poiLocation) {
 		}
 	}
-	
-    @Override  
-    public void onDestroy() {  
-        super.onDestroy();  
-        
-        /*退出时保存这次的定位信息*/
-        settings.edit().putLong("Lat", (long)lat).commit();
-        settings.edit().putLong("Lng", (long)lng).commit();
-        
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+
+		/* 退出时保存这次的定位信息 */
+		settings.edit().putLong("Lat", (long) lat).commit();
+		settings.edit().putLong("Lng", (long) lng).commit();
+
 		// 退出时销毁定位
 		mLocClient.stop();
 		// 关闭定位图层
 		mBaiduMap.setMyLocationEnabled(false);
 		mMapView.onDestroy();
 		mMapView = null;
-		
-    }  
-    
+	}
+
 	@Override
 	public void onResume() {
 		super.onResume();
 		// 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
 		mMapView.onResume();
 	}
-	
-    @Override  
-    public void onPause() {  
-        super.onPause();  
-        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理  
-        mMapView.onPause();  
-    }
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		// 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+		mMapView.onPause();
+	}
 }
