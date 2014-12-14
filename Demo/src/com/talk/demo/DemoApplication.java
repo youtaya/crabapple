a1778924ea4b4d2817ee5554bf9c5c2d108e700b
youtaya <jxiaoping@gmail.com>
2014-12-14 13:47:00 +0800

    add push mach

--- Demo/src/com/talk/demo/DemoApplication.java
+++ Demo/src/com/talk/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.talk.demo;
 
 import android.app.Application;
 
+import cn.jpush.android.api.JPushInterface;
+
 import com.baidu.mapapi.SDKInitializer;
 
 public class DemoApplication extends Application {
@@ -11,6 +13,9 @@ public class DemoApplication extends Application {
 		super.onCreate();
 		// 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
 		SDKInitializer.initialize(this);
+		
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
 	}
 
 }
