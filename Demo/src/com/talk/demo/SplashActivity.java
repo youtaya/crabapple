cc4e78ed402646e963488b1b81d9447d969d90b6
youtaya <jxiaoping@gmail.com>
2015-01-05 22:31:06 +0800

    update

---
+++ Demo/src/com/talk/demo/SplashActivity.java
@@ -0,0 +1,46 @@
+
+package com.talk.demo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.talk.demo.account.AppEnterActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+    private static final long DELAY = 3000;
+    private boolean scheduled = false;
+    private Timer splashTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lanuch);
+
+        splashTimer = new Timer();
+        splashTimer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+            	SplashActivity.this.finish();
+                startActivity(new Intent(SplashActivity.this, AppEnterActivity.class));
+                //overridePendingTransition(R.anim.fade, R.anim.hold);
+            }
+        }, DELAY);
+        scheduled = true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (scheduled)
+            splashTimer.cancel();
+        splashTimer.purge();
+    }
+}
